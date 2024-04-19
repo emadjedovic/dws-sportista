96a020a6678ba2fb843ece1a42336f38f6380eb4
Ema Djedovic <ema.djedovic27@gmail.com>
2024-04-19 10:35:01 +0200

    Dobar dio schemas.py i models.py završen, radi aplikacija!!
    U schemas.py dodane neke Pydantic validacije podataka, dati prijedlozi za rješavanje problema s relationships.
    
    U models.py zamijenili datetime sa DateTime. Također prijedlog da ipak koristimo SQLite.

--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -1,18 +1,20 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field, constr, validator
 from datetime import datetime
+
 """
 1Bazne klase1 sadrze sve podatke
 Nasljedju BaseModel
 """
+
 class KorisnikBase(BaseModel):
 
     sifra: str
     ime: str
     prezime: str
-    datum_rodenja: datetime
+    datum_rodjenja: datetime
     lokacija: str
-    mail: str
+    mail: EmailStr
     telefon:str
 
 class VlasnikBase(BaseModel):
@@ -20,9 +22,9 @@ class VlasnikBase(BaseModel):
     sifra: str
     ime: str
     prezime: str
-    datum_rodenja: datetime
+    datum_rodjenja: datetime
     lokacija: str
-    mail: str
+    mail: EmailStr
     telefon:str   
 
     # 1:N rel
@@ -39,7 +41,7 @@ class TerenBase(BaseModel):
     ocjene_id: int
     vrsta: str
     lokacija: str
-    cijena: str
+    cijena: str #ili int??
     # 1:N
     # 1:1
     # 1:1
@@ -49,7 +51,7 @@ class OcjeneBase(BaseModel):
 
     korisnik_id: int
     teren_id: int
-    ocjena: int
+    ocjena: int = Field(..., ge=0, le=5) # od 0 do 5
 
     # 1:1
 
@@ -61,6 +63,12 @@ class TerminBase(BaseModel):
     vrijeme_kraja: datetime
     je_li_privatni: bool
 
+    @validator('vrijeme_kraja')
+    def end_time_after_start_time(cls, v, values):
+        if 'vrijeme_pocetka' in values and v <= values['vrijeme_pocetka']:
+            raise ValueError('Vrijeme kraja mora biti nakon vremena početka.')
+        return v
+
     # 1:N
     # 1:N
 
@@ -68,10 +76,10 @@ class TimBase(BaseModel):
     
     sport_id: int
     termin_id: int
-    broj_igraca: int
-    nivo_vjestine: int
+    broj_igraca: int #je li ovo maksimalan broj igraca ili potreban broj igraca?
+    nivo_vjestine: int = Field(..., ge=0, le=5) # od 0 do 5
     lokacija_tima: str
-    broj_slobodnih_mjesta: int
+    broj_slobodnih_mjesta: int = Field(..., ge=0)
 
     # 1:N
 
@@ -90,66 +98,73 @@ class KorisnikRead(KorisnikBase):
     id: int
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 class VlasnikRead(VlasnikBase):
 
     id: int    #RELACIJA PROBLEM
-    tereni = Optional[TerenRead] = None
+    # Mislim kako nemamo atribut za sve terene tog vlasnika da ne mozemo ni
+    # ispisati sve njegove terene? To bismo mogli iz tabele Teren kao "select * gdje je vlasnik taj i taj..."
+    # tereni = Optional[TerenRead] = None
 
     class Config: 
-        orm_mode = True
+        from_attributes= True
 
 class SportRead(SportBase):
 
     id: int     #RELACIJA PROBLEM
-    teren: Optional[TerenRead] = None
+    # one-to-many, jedan sport moze imati vise terena, a oni bi se mogli izlistati iz
+    # tabele Teren kao "select * where sport je taj i taj..."
+    # teren: Optional[TerenRead] = None
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 class TerenRead(TerenBase):
 
     id: int  #RELACIJA PROBLEM?, STA CE BITI VlasnikRead, SportRead
-    vlasnik: Optional[VlasnikRead] = None
-    sport: Optional[SportRead]= None
-    ocjene: Optional[Ocjene]= None
-    termini: Optional[Termin]= None
+    # vlasnik: Optional[VlasnikRead] = None -- imamo vlasnik_id
+    # sport: Optional[SportRead]= None -- ispisat ce se svejedno sport_id, to nam je dovoljno? nisam 100% sigurna
+    # ocjene: Optional[Ocjene]= None -- isto ocjene_id ce bit, uvijek mozemo korisiti "select" upite da izlistamo
+    # sve ocjene na zahtjev?
+    
+    # termini: Optional[Termin]= None -- necemo sve termine ispisivati, mozemo u "Termini" odraditi
+    # select * where teren=eldinovTeren
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 class OcjeneRead(OcjeneBase):
 
     id: int
-    teren: Optional[TerenRead] = None
+    # teren: Optional[TerenRead] = None -- ispisat ce nam teren_id svejedno
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 class TerminRead(TerminBase):
 
     id: int  #RELACIJA PROBLEM?
-    teren: Optional[TerenRead] = None  
-    tim: Optional[TimRead] = None 
+    # teren: Optional[TerenRead] = None  -- teren_id
+    # tim: Optional[TimRead] = None -- tim_id
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 class TimRead(TimBase):
 
     id: int
-    termini: Optional[TerminRead] = None
+    # termini: Optional[TerminRead] = None -- "select * from Termin where tim=eldinovTim" i izlistat ce sve :D
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 class KorisnikTimRead(KorisnikTimBase):
 
     id: int
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 """
 Klase za kreiranje podataka, 
