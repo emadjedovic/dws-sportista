504fc790e60c61e90d2221b752a1dc22d1e691a9
unknown <andrijana_md@hotmail.com>
2024-04-29 00:12:36 +0200

    povezano sa endpointom

--- app/src/Components/LoginSignup.js
+++ app/src/Components/LoginSignup.js
@@ -1,28 +1,65 @@
 import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client'
-import Box from '@mui/material/Box';
-import FormControl from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
-import PersonIcon from '@mui/icons-material/Person';
-import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import {FormControl,TextField,Chip,Box,Switch} from '@mui/material';
+/*import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 import PasswordIcon from '@mui/icons-material/Password';
-import { Chip } from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import PersonPinCircleIcon from '@mui/icons-material/PersonPinCircle';
-import PhoneIcon from '@mui/icons-material/Phone';
-
-
-
+import PhoneIcon from '@mui/icons-material/Phone';*/
+import axios from 'axios';
 
 import './LoginSignup.css';
-/*import Login from './Login.jsx';
-import Signup from './SignUp.jsx';*/
 
+// Komponenta za registraciju korisnika
 const Signup = ({ toggleMode }) => {
-  const [selectedSports, setSelectedSports] = useState([]);
-  const [currentSport, setCurrentSport] = useState('');
-  const [availableSports, setAvailableSports] = useState(['Fudbal', 'Košarka', 'Tenis', 'Odbojka']);
 
+  // Stanje za podatke korisnika
+  const [userData, setUserData] = useState({
+    username: '',
+    ime: '',
+    prezime: '',
+    email: '',
+    password: '',
+    datum_rodjenja: '',
+    lokacija: '',
+    telefon: '',
+    izabraniSportovi: [],
+  });
+
+  // Stanje za izabrane sportove
+  const [selectedSports, setSelectedSports] = useState([]);
+  // Stanje za trenutni sport
+  const [currentSport, setCurrentSport] = useState('');
+  // Stanje za dostupne sportove (trebao bi endpoint za dohvatanje sportova iz baze)
+  const [availableSports, setAvailableSports] = useState(['Fudbal', 'Košarka', 'Tenis', 'Odbojka']);
+  // Stanje za profil tipa poslovni ili korisnik
+  const [poslovni, setPoslovni] = React.useState(false);
+
+  // Funkcija za popunjavanje userData
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
+    };
+
+    // Funkcija za odabir poslovnog tipa profila
+    const handleChange = (e) => {
+      setPoslovni(e.target.checked);
+      console.log(poslovni)
+    };
+
+  // Funkcija za registraciju
+  const handleSubmit = async (e) => {
+    e.preventDefault(); 
+    try {
+      if(!poslovni){
+      const response = await axios.post('http://localhost:8000/register', userData);
+      console.log(response.data);} 
+    } catch (error) {
+      console.error('Greška pri registraciji korisnika:', error);
+    }
+  };
+
+  // Funkcija za dodavanje izabranog sporta (ako nije već odabrano 3 sporta)
   const handleSportSelect = (sport) => {
     if (selectedSports.length < 3 && !selectedSports.includes(sport)) {
       setSelectedSports([...selectedSports, sport]);
@@ -30,14 +67,16 @@ const Signup = ({ toggleMode }) => {
     }
   };
 
+  // Funkcija za uklanjanje izabranog sporta
   const handleSportRemove = (sportToRemove) => {
     setSelectedSports(selectedSports.filter(sport => sport !== sportToRemove));
   };
 
+  // Prikaz forme za registraciju
   return (
-    <div className='container'>
+    <form className='form' onSubmit={handleSubmit} >
       <div className='header'>
-        <div className='text'>Sign Up</div>
+        <div className='text'>Registracija</div>
       </div>
       <div className='inputs'>
         <Box
@@ -53,90 +92,96 @@ const Signup = ({ toggleMode }) => {
         >
           <FormControl variant="standard">
             <TextField
-              id="name"
-              label="Name"
-              InputProps={{
-              
-                startAdornment: (
-                  <PersonIcon />
-                ),
-              }}
+              id="username"
+              label="Korisničko ime"
+              name="username"
+              value={userData.username}
+              onChange={handleInputChange}
+              required
             />
           </FormControl>
           <FormControl variant="standard">
             <TextField
-              id="last-name"
-              label="Last name"
+              id="ime"
+              label="Ime"
+
+              name="ime"
+              value={userData.ime}
+              onChange={handleInputChange}
+              required
+            />
+          </FormControl>
+          <FormControl variant="standard">
+            <TextField
+              id="prezime"
+              label="Prezime"
+              name="prezime"
+              value={userData.prezime}
+              onChange={handleInputChange}
+              required
             />
           </FormControl>
           <FormControl variant="standard">
             <TextField
               id="email"
               label="E-mail"
-              InputProps={{
-                startAdornment: (
-                  <AlternateEmailIcon />
-                ),
-              }}
+
+              name="email"
+              value={userData.email}
+              onChange={handleInputChange}
+              required
             />
           </FormControl>
           <FormControl variant="standard">
             <TextField
               id="password"
-              label="Password"
+              label="Lozinka"
               type="password"
-              InputProps={{
-                
-                startAdornment: (
-                  <PasswordIcon />
-                ),
-              }}
+              name="password"
+              value={userData.password}
+              onChange={handleInputChange}
+              required
             />
           </FormControl>
           <FormControl variant="standard">
             <TextField
-              id="date_of_birth"
-              placeholder="Date of birth"
-              label="Date of birth"
+              id="datum_rodjenja"
+              placeholder="Datum rođenja"
               type="date"
-              InputProps={{
-                startAdornment: (
-                  <CalendarMonthIcon/>
-                ),
-              }}
+              name="datum_rodjenja"
+              value={userData.datum_rodjenja}
+              onChange={handleInputChange}
+              required
+            />
+          </FormControl>
+          <FormControl variant="standard">
+            <TextField
+              id="lokacija"
+              label="Lokacija"
+              name="lokacija"
+              value={userData.lokacija}
+              onChange={handleInputChange}
+              required
+            />
+          </FormControl>
+          <FormControl variant="standard">
+            <TextField
+              id="telefon"
+              label="Telefon"
               
-            />
-          </FormControl>
-          <FormControl variant="standard">
-            <TextField
-              id="location"
-              label="Location"
-              InputProps={{
-                startAdornment: (
-                  <PersonPinCircleIcon />
-                ),
-              }}
-            />
-          </FormControl>
-          <FormControl variant="standard">
-            <TextField
-              id="phone"
-              label="Phone"
-              InputProps={{
-                startAdornment: (
-                  <PhoneIcon/>
-                ),
-              }}
+              name="telefon"
+              value={userData.telefon}
+              onChange={handleInputChange}
+              
             />
           </FormControl>
 
           <Box component="div" className="select-sport-div">
-              <label className='select-sport-label'>Select up to 3 sports</label>
-              
+            <label className='select-sport-label'>Izaberite do 3 sporta:</label>
             <Box
               component="fieldset"
               id="sports"
-              value={selectedSports.join(', ')} 
+              value={selectedSports.join(', ')}
               onChange={(e) => setCurrentSport(e.target.value)}
               style={{
                 position: 'relative',
@@ -146,7 +191,7 @@ const Signup = ({ toggleMode }) => {
                 borderRadius: '4px',
                 border: '1px solid #b2abab',
                 resize: 'none',
-                caretColor: 'initial', 
+                caretColor: 'initial',
               }}
             >
               <div
@@ -175,24 +220,58 @@ const Signup = ({ toggleMode }) => {
               ))}
             </Box>
           </Box>
+          <div className='poslovni-switch'>
+            <label>Poslovni</label>
+              <Switch
+              label='Poslovni'
+                checked={poslovni}
+                onChange={handleChange}
+                inputProps={{ 'aria-label': 'controlled' }}
+              />
+          </div>
         </Box>
       </div>
-      <div className='forgot-password'>Already have an account? <span onClick={toggleMode}>Sign in!</span></div>
+      <div className='account-check'>Već imate nalog? <span onClick={toggleMode}>Prijavite se!</span></div>
       <div className='submit-container'>
-        <div className='submit'>Sign Up</div>
+        <button type="submit" className='submit'>Registracija</button>
       </div>
-    </div>
+    </form>
   );
 };
 
-
+// Komponenta za prijavu korisnika
 const Login = ({ toggleMode }) => {
+
+  const formData = new FormData();
+
+  // Funkcija za popunjavanje objekta formData podacima korisnika
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    formData.set(name, value); 
+    }
+
+  // Funkcija za prijavu
+  const handleSubmit = async (e) => {
+    e.preventDefault(); 
+    try {
+      const response = await axios.post('http://localhost:8000/token', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      }); 
+      console.log("Uspješan login!\n", response.data);
+    } catch (error) {
+      console.error('Greška pri prijavi korisnika:', error);
+    }
+  }
+  
+  // Forma za prijavu
   return (
-    <div className='container'>
+    <form className='container' onSubmit={handleSubmit}>
       <div className='header'>
-        <div className='text'>Login</div>
+        <div className='text'>Prijava</div>
       </div>
-      <div className='inputs'>
+      <div className='inputs' >
         <Box
           sx={{
             display: 'flex',
@@ -206,50 +285,54 @@ const Login = ({ toggleMode }) => {
         >
           <FormControl variant="standard">
             <TextField
-              id="input-with-icon-adornment"
-              label="E-mail"
-              InputProps={{
-                startAdornment: (
-                  <AlternateEmailIcon />
-                ),
-              }}
+              id="username"
+              label="Korisničko ime"
+              name="username"
+              onChange={handleInputChange}
+              required
             />
           </FormControl>
           <FormControl variant="standard">
             <TextField
-              id="input-with-icon-adornment"
-              label="Password"
-              InputProps={{
-                startAdornment: (
-                  <PasswordIcon />
-                ),
-              }}
+              id="password"
+              type="password"
+              label="Lozinka"
+              name="password"
+              onChange={handleInputChange}
+              required
             />
           </FormControl>
         </Box>
       </div>
-      <div className='forgot-password'>Don't have an account? <span onClick={toggleMode}>Sign up!</span></div>
+      <div className='account-check'>Nemate nalog? <span onClick={toggleMode}>Registrujte se!</span></div>
       <div className='submit-container'>
-        <div className='submit'>Sign In</div>
+        <button type='submit' className='submit'>Prijavite se</button>
       </div>
-    </div>
+    </form>
   );
 };
 
+// Komponenta za prijavu/registraciju
 const LoginSignup = () => {
+
+    // Stanje za praćenje režima (prijavljivanje ili registracija)
   const [isSignUp, setIsSignUp] = useState(false);
 
+  // Funkcija za promjenu režima
   const toggleMode = () => {
     setIsSignUp(!isSignUp);
   };
 
+  // Dodjeljivanje komponente u zavisnosti od režima
   let component;
   if (!isSignUp) {
     component = <Login toggleMode={toggleMode} />;
   } else {
     component = <Signup toggleMode={toggleMode} />;
   }
-
+  
+  // Prikaz komponente
+  window.scrollTo(0, 0)
   return (
     <>
       {component}
