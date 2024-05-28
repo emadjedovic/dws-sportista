a138ee5b3060b7ce75a8421d586812d67da91b79
melisah00 <155679175+melisah00@users.noreply.github.com>
2024-05-28 18:53:22 +0200

    Update index.js

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import './index.css';
+import './index.css';
 import App from './App';
-import Home from './Pages/Home/Home';
+//import Home from './Pages/Home/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>
 );
